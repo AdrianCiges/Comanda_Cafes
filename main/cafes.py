37dda357ae0b8995cc273d9ec11accb680d69c5a
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-05-19 13:54:24 +0200

    Update cafes.py

--- main/cafes.py
+++ main/cafes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from collections import Counter
 
-st.set_page_config(layout="wide", page_title="Coffees", page_icon="./img/cafe3.png")
+st.set_page_config(layout="wide", page_title="Coffees", page_icon="./img/cafe4.png")
 st.markdown('<h1 style="text-align:center"><span style="font-size: 40px;">☕</span> <u>LA RUTA DEL BUEN CAFÉ</u></h1>', unsafe_allow_html=True)
 
 # Botón para añadir una persona más
