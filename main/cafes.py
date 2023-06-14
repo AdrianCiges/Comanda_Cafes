bf74b42c15129eb3e9f3695c228c2f0846fe6c71
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-06-14 14:59:37 +0200

    Update cafes.py

--- main/cafes.py
+++ main/cafes.py
@@ -455,6 +455,8 @@ try:
         output = []
 
         output.append('Hola! Os hago un pedido:\n')
+        
+        normales = ccl - ccl_sinlact - ccl_desnat
 
         if ccl > 0:
 
@@ -464,8 +466,9 @@ try:
                     output.append(f'   - {ccl_sinlact} sin lactosa')
                 if ccl_desnat > 0:
                     output.append(f'   - {ccl_desnat} desnatada')
-                if (ccl - (ccl_sinlat+ccl_desnat)) > 0:
-                    output.append(f'   - {(ccl - (ccl_sinlat+ccl_desnat))} normales')
+                if normales > 0:
+                    output.append(f'   - {normales} normales')
+
             else:
                 output.append(f'• {ccl} café con leche')
 
