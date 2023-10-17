f05d3174bb5d105db2096fab03cdb6895468edb4
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-17 12:11:36 +0200

    Update cafes.py

--- main/cafes.py
+++ main/cafes.py
@@ -462,7 +462,9 @@ try:
 
         if ccl > 0:
 
+            st.write('Funciono aún')
             st.write(f'{output}')
+            st.write(f'{ccl}')
 
             if ccl_sinlact > 0 or ccl_desnat > 0 or ccl_almendra > 0 or ccl_avena > 0:
                 output.append(f'• {ccl} café con leche, de los cuales:')
