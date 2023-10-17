50f3af076cbf4d26219bbb270afdfe9439b9b2c0
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-17 09:56:08 +0200

    Update cafes.py
    pruebas1

--- main/cafes.py
+++ main/cafes.py
@@ -81,33 +81,6 @@ with io.BytesIO() as output:
     
 # st.markdown(f'<h1 style="text-align:center"><span style="font-size: 40px;">☕</span> <u>LA RUTA DEL BUEN CAFÉ</u></h1>', unsafe_allow_html=True)
 
-# ----------------------------------------------------------------------------------------
-
-# # Ruta de la imagen del logo
-# LOGO_IMAGE = "./img/havas.png"
-
-# # Texto principal
-# texto_principal = '<h1 style="text-align:center"><span style="font-size: 40px;">☕</span> <u>LA RUTA DEL BUEN CAFÉ</u></h1>'
-
-# # Estilos CSS para el logo
-# estilos_css = f"""
-#     <style>
-#     .logo-container {{
-#         display: flex;
-#         align-items: center;
-#         justify-content: center;
-#     }}
-
-#     .logo-img {{
-#         max-height: 100px;
-#         margin-left: 20px;
-#     }}
-#     </style>
-#     """
-
-# # Mostrar el texto principal y el logo
-# st.markdown(estilos_css, unsafe_allow_html=True)
-# st.markdown(f'<div class="logo-container">{texto_principal}<img src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}" class="logo-img"></div>', unsafe_allow_html=True)
 
 # ----------------------------------------------------------------------------------------
 
@@ -385,6 +358,8 @@ try:
         otros = 0
 
         que = {}
+
+        st.write(conteo)
 
         for k, v in conteo.items():
 
