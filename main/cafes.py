9580c9e8cc0c39b7a3bf97d222283d97f949ad2a
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-19 20:33:48 +0200

    Update cafes.py

--- main/cafes.py
+++ main/cafes.py
@@ -10,93 +10,50 @@ import re
 
 st.set_page_config(layout="wide", page_title="Coffees", page_icon="./img/cafe5.png")
 
-# -------------------------------------------------------------------------------------------------------------------
-tab1, tab2 = st.tabs(["Havas", "Others"])
+# havas = True
 
-with tab1:
+if havas:
+    
+    hora_actual = datetime.now().time()
+    hora_actual_dt = datetime.combine(datetime.today(), hora_actual)
+    hora_sumada = hora_actual_dt + timedelta(hours=2)
+    
+    hora_actual = hora_sumada.time()
+    hora_objetivo = time(10, 30)
+    primer_cafe = time(10, 30)
+    segundo_cafe = time(14, 45)
 
-    havas = True
-
-    if havas:
-    
-        hora_actual = datetime.now().time()
-        hora_actual_dt = datetime.combine(datetime.today(), hora_actual)
-        hora_sumada = hora_actual_dt + timedelta(hours=2)
-        
-        hora_actual = hora_sumada.time()
-        hora_objetivo = time(10, 30)
-        primer_cafe = time(10, 30)
-        segundo_cafe = time(14, 45)
-    
-        if hora_actual < hora_objetivo:
-            tiempo_restante = datetime.combine(datetime.today(), hora_objetivo) - datetime.combine(datetime.today(), hora_actual)
-        else:
-            # Sumamos un día al tiempo objetivo para obtener la próxima ocurrencia
-            tiempo_restante = datetime.combine(datetime.today() + timedelta(days=1), hora_objetivo) - datetime.combine(datetime.today(), hora_actual)
-        
-        horas_restantes = tiempo_restante.seconds // 3600
-        minutos_restantes = (tiempo_restante.seconds % 3600) // 60
-        
-        if hora_actual < segundo_cafe:
-            tiempo_restante2 = datetime.combine(datetime.today(), segundo_cafe) - datetime.combine(datetime.today(), hora_actual)
-        else:
-            # Sumamos un día al tiempo objetivo para obtener la próxima ocurrencia
-            tiempo_restante2 = datetime.combine(datetime.today() + timedelta(days=1), segundo_cafe) - datetime.combine(datetime.today(), hora_actual)
-        
-        horas_restantes2 = tiempo_restante2.seconds // 3600
-        minutos_restantes2 = (tiempo_restante2.seconds % 3600) // 60
-        
-        if hora_actual > time(18, 00):
-            st.sidebar.write(f'Mira que horas son, no deberías estar aquí, pero faltan {horas_restantes} horas y {minutos_restantes} minutos para el ☕ de las 10:30')
-        elif hora_actual < primer_cafe:
-            st.sidebar.write(f"{horas_restantes} horas y {minutos_restantes} minutos para el ☕ de las 10:30")
-        elif hora_actual < time(11, 00):
-            st.sidebar.write('Ya deberías estar tomándote un café con los compis')
-        elif hora_actual < segundo_cafe:
-            st.sidebar.write(f"{horas_restantes2} horas y {minutos_restantes2} minutos para el ☕ de las 14:45")
-        else:
-            st.sidebar.write('No hay más cafés hoy ☹')
-    
-    # -------------------------------------------------------------------------------------------------------------------
-    
-    # Cambiar el tema de la barra lateral
-    st.markdown(
-        """
-        <style>
-        .sidebar .sidebar-content {
-            background-color: #f8f9fa;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    
-    # Cambiar el tema de la página principal
-    st.markdown(
-        """
-        <style>
-        .stApp {
-            background-color: #e9ecef;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    
-    # image_inicio = Image.open("./img/havas.png")
-    # with io.BytesIO() as output:
-    #     image_inicio.save(output, format="PNG")
-    #     b64_1 = base64.b64encode(output.getvalue()).decode()
-    
-    
-    # ----------------------------------------------------------------------------------------
-
-    if havas:
-        # Ruta de la imagen del logo
-        LOGO_IMAGE = "./img/havas.png"
+    if hora_actual < hora_objetivo:
+        tiempo_restante = datetime.combine(datetime.today(), hora_objetivo) - datetime.combine(datetime.today(), hora_actual)
     else:
-        pass
+        # Sumamos un día al tiempo objetivo para obtener la próxima ocurrencia
+        tiempo_restante = datetime.combine(datetime.today() + timedelta(days=1), hora_objetivo) - datetime.combine(datetime.today(), hora_actual)
+    
+    horas_restantes = tiempo_restante.seconds // 3600
+    minutos_restantes = (tiempo_restante.seconds % 3600) // 60
+    
+    if hora_actual < segundo_cafe:
+        tiempo_restante2 = datetime.combine(datetime.today(), segundo_cafe) - datetime.combine(datetime.today(), hora_actual)
+    else:
+        # Sumamos un día al tiempo objetivo para obtener la próxima ocurrencia
+        tiempo_restante2 = datetime.combine(datetime.today() + timedelta(days=1), segundo_cafe) - datetime.combine(datetime.today(), hora_actual)
+    
+    horas_restantes2 = tiempo_restante2.seconds // 3600
+    minutos_restantes2 = (tiempo_restante2.seconds % 3600) // 60
+    
+    if hora_actual > time(18, 00):
+        st.sidebar.write(f'Mira que horas son, no deberías estar aquí, pero faltan {horas_restantes} horas y {minutos_restantes} minutos para el ☕ de las 10:30')
+    elif hora_actual < primer_cafe:
+        st.sidebar.write(f"{horas_restantes} horas y {minutos_restantes} minutos para el ☕ de las 10:30")
+    elif hora_actual < time(11, 00):
+        st.sidebar.write('Ya deberías estar tomándote un café con los compis')
+    elif hora_actual < segundo_cafe:
+        st.sidebar.write(f"{horas_restantes2} horas y {minutos_restantes2} minutos para el ☕ de las 14:45")
+    else:
+        st.sidebar.write('No hay más cafés hoy ☹')
+
+    # Ruta de la imagen del logo
+    LOGO_IMAGE = "./img/havas.png"
     
     # Texto principal
     texto_principal = '<h1 style="text-align:center"><span style="font-size: 40px;">☕</span> <u>LA RUTA DEL CAFÉ</u></h1>'
@@ -142,6 +99,83 @@ with tab1:
         for i in range(nuevos):
             gente_nueva.append(col1.text_input(f'Nombre {i+1}'))
         gente_nueva = [g for g in gente_nueva if g != '']
+
+else:
+
+    # Ruta de la imagen del logo
+    LOGO_IMAGE = "./img/havas.png"
+    
+    # Texto principal
+    texto_principal = '<h1 style="text-align:center"><span style="font-size: 40px;">☕</span> <u>LA RUTA DEL CAFÉ</u></h1>'
+    
+    # Estilos CSS para el logo y el contenedor
+    estilos_css = f"""
+        <style>
+        .logo-container {{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }}
+    
+        .logo-img {{
+            height: 40px;
+            width: auto;
+            margin-left: 20px;
+        }}
+        </style>
+        """
+        
+    # Leer la imagen del logo y codificarla en base64
+    with open(LOGO_IMAGE, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode()
+    
+    # Mostrar el texto principal y el logo
+    st.markdown(estilos_css, unsafe_allow_html=True)
+    st.markdown(
+        f'<div class="logo-container">{texto_principal}<img src="data:image/png;base64,{encoded_image}" class="logo-img"></div>',
+        unsafe_allow_html=True
+    )
+        
+# -------------------------------------------------------------------------------------------------------------------
+
+tab1, tab2 = st.tabs(["Havas", "Others"])
+
+with tab1:
+
+    havas = True
+        
+    # Cambiar el tema de la barra lateral
+    st.markdown(
+        """
+        <style>
+        .sidebar .sidebar-content {
+            background-color: #f8f9fa;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    # Cambiar el tema de la página principal
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background-color: #e9ecef;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    
+    # image_inicio = Image.open("./img/havas.png")
+    # with io.BytesIO() as output:
+    #     image_inicio.save(output, format="PNG")
+    #     b64_1 = base64.b64encode(output.getvalue()).decode()
+    
+    
+    # ----------------------------------------------------------------------------------------
     
     def gente():
         personas = ['Adrián', 'Álvaro D.', 'Álvaro S.', 'Ana G.', 'Ana M.', 'Dani A.', 'Dani S.', 'Dasha', 'Inés MG', 'Inés ML', 'Javi B.', 'Javi N.', 'Lucas', 'Lucía', 'María E.', 'María L.', 'Maxi', 'Mercedes', 'Rafa', 'Rosalía', 'Rubén C.', 'Rubén I.', 'Sergio', 'Víctor' ]   
@@ -901,10 +935,10 @@ with tab1:
     col3.metric("% Hoy vs Habitual", f'{perc_hab}%', f"{perc_hab-100}% de lo habitual")
     col4.metric("Media habitual", 6)
 
+# -------------------------------------------------------------------------------------------------------------------
+
 with tab2:
     havas = False
-    
-    # -------------------------------------------------------------------------------------------------------------------
     
     # Cambiar el tema de la barra lateral
     st.markdown(
@@ -929,44 +963,6 @@ with tab2:
         """,
         unsafe_allow_html=True
     )
-    
-    
-    # ----------------------------------------------------------------------------------------
-    
-    
-    # Ruta de la imagen del logo
-    # LOGO_IMAGE = "./img/havas.png"
-    
-    # Texto principal
-    texto_principal = '<h1 style="text-align:center"><span style="font-size: 40px;">☕</span> <u>LA RUTA DEL CAFÉ</u></h1>'
-    
-    # Estilos CSS para el logo y el contenedor
-    estilos_css = f"""
-        <style>
-        .logo-container {{
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }}
-    
-        .logo-img {{
-            height: 40px;
-            width: auto;
-            margin-left: 20px;
-        }}
-        </style>
-        """
-        
-    # Leer la imagen del logo y codificarla en base64
-    # with open(LOGO_IMAGE, "rb") as image_file:
-    #     encoded_image = base64.b64encode(image_file.read()).decode()
-    
-    # Mostrar el texto principal y el logo
-    # st.markdown(estilos_css, unsafe_allow_html=True)
-    # st.markdown(
-    #     f'<div class="logo-container">{texto_principal}<img src="data:image/png;base64,{encoded_image}" class="logo-img"></div>',
-    #     unsafe_allow_html=True
-    # )
     
     # ----------------------------------------------------------------------------------------
     
