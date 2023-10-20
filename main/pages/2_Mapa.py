ba5735e41b3546570007f1d8e8c3c4fe370fa77d
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-20 17:15:14 +0200

    Update 2_Mapa.py

--- main/pages/2_Mapa.py
+++ main/pages/2_Mapa.py
@@ -47,27 +47,27 @@ estilos_css = f"""
     </style>
     """
 
-# --------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------FUNCIONA-------------------------------------
 
-loc_button = Button(label="Mi ubicación")
-loc_button.js_on_event("button_click", CustomJS(code="""
-    navigator.geolocation.getCurrentPosition(
-        (loc) => {
-            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
-        }
-    )
-    """))
-result = streamlit_bokeh_events(
-    loc_button,
-    events="GET_LOCATION",
-    key="get_location",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
+# loc_button = Button(label="Mi ubicación")
+# loc_button.js_on_event("button_click", CustomJS(code="""
+#     navigator.geolocation.getCurrentPosition(
+#         (loc) => {
+#             document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+#         }
+#     )
+#     """))
+# result = streamlit_bokeh_events(
+#     loc_button,
+#     events="GET_LOCATION",
+#     key="get_location",
+#     refresh_on_update=False,
+#     override_height=75,
+#     debounce_time=0)
 
-if result:
-    if "GET_LOCATION" in result:
-        st.write(result.get("GET_LOCATION"))
+# if result:
+#     if "GET_LOCATION" in result:
+#         st.write(result.get("GET_LOCATION"))
 
 # --------------------------------------------------------------------------------------------------------------------
 
