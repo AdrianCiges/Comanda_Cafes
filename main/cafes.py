34eb00cb4a5793d0da9b709ab977b2f880373924
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-20 10:22:22 +0200

    Update cafes.py
    prueba métricas

--- main/cafes.py
+++ main/cafes.py
@@ -1594,4 +1594,8 @@ with tab2:
     col1.metric("Cafeteros hoy", n_cafeteros, f"{n_cafeteros-6} de lo habitual")
     col2.metric("% Hoy vs Total", f'{perc_total}%', f"{perc_total-100}% del total")
     col3.metric("% Hoy vs Habitual", f'{perc_hab}%', f"{perc_hab-100}% de lo habitual")
-    col4.metric("Media habitual", 6)
+    # col4.metric("Media habitual", 6)
+
+    insert_number = st.number_input('Insert a number:', value=6)
+    st.metric("Media habitual", insert_number)
+
