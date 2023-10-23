5715ddff48daa03e8a568a6e6ff056e6297567b1
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-23 13:19:19 +0200

    Update 2_Mapa.py

--- main/pages/2_Mapa.py
+++ main/pages/2_Mapa.py
@@ -88,8 +88,7 @@ def extract_cafeterias_in_madrid():
             "Horario": node.tags.get("opening_hours", "-"),
             "Terraza": node.tags.get("outdoor_seating", "DESCONOCIDO"),
             "Latitude": float(node.lat),
-            "Longitude": float("{:.20f}".format(node.lon))
-            
+            "Longitude": float(node.lon)
         }
         cafes.append(cafe_info)
 
