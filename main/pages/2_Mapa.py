4d8835e4dc794054fa6656ac18fb53e31842bcd5
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-23 09:49:45 +0200

    Update 2_Mapa.py

--- main/pages/2_Mapa.py
+++ main/pages/2_Mapa.py
@@ -22,6 +22,7 @@ from bokeh.palettes import RdBu3
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 import math
+import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Ruta del Café", page_icon="./img/cafe5.png")
 
