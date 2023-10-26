68f4e5b2fb13824a303ef0fb4e27cfe3d3a2ebf7
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-26 17:36:58 +0200

    Update Havas.py

--- main/Havas.py
+++ main/Havas.py
@@ -3,8 +3,8 @@ from st_pages import Page, show_pages, add_page_title
 
 show_pages(
     [
-        Page("pags/Havas.py", "HAVAS group", "💻"),  
-        Page("pags/Tu_grupo.py", "NEW group", "⚙️"),  
-        Page("pags/Mapa.py", "Localizador", "🌍")    
+        Page("pags/0_Havas.py", "HAVAS group", "💻"),  
+        Page("pags/1_Tu_grupo.py", "NEW group", "⚙️"),  
+        Page("pags/2_Mapa.py", "Localizador", "🌍")    
     ]
 )
