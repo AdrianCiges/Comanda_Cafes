28c334504ffdf646e78e08ea33c7fdbb2da87b48
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-26 17:37:33 +0200

    Update Havas.py

--- main/Havas.py
+++ main/Havas.py
@@ -1,5 +1,17 @@
 import streamlit as st
 from st_pages import Page, show_pages, add_page_title
+from collections import Counter
+from PIL import Image
+import base64
+import io
+import datetime
+from datetime import datetime, time, timedelta
+import re
+import streamlit.components.v1 as components
+import time as timee
+#import pywhatkit
+
+st.set_page_config(layout="wide", page_title="Ruta del Café", page_icon="./img/cafe5.png")
 
 show_pages(
     [
