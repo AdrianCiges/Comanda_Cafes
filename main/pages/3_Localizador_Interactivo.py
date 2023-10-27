04fbfc1815378f06a1abab855843f9245550e7be
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 16:03:45 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,7 +1,26 @@
 import streamlit as st
-import streamlit_js_eval
+import leafmap.foliumap as leafmap
 
-# Returns user's location after asking for permission when the user clicks the generated link with the given text
-location = get_geolocation()
-# The URL parts of the page
-location_json = get_page_location()
+# Título de la aplicación
+st.title("Geolocalización del Usuario con Leafmap en Streamlit")
+
+# Crear un mapa interactivo con Leafmap
+m = leafmap.Map(zoom_control=False, draw_export=False, locate_control=True)
+
+# Mostrar el mapa en la aplicación
+st.write(m)
+
+# Función para obtener la geolocalización del usuario
+def get_user_location():
+    user_location = m.get_location()
+    if user_location:
+        latitude = user_location["lat"]
+        longitude = user_location["lng"]
+        st.write(f"Latitud: {latitude:.6f}")
+        st.write(f"Longitud: {longitude:.6f}")
+    else:
+        st.warning("La geolocalización no está habilitada o no se pudo obtener.")
+
+# Botón para obtener la geolocalización del usuario
+if st.button("Obtener Geolocalización"):
+    get_user_location()
