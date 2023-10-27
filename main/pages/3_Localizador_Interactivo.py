2a6892c59824a00b5ff56d52b12c2c5b63f21753
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 16:16:19 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,40 +1,12 @@
 import streamlit as st
 from streamlit.components.v1 import ComponentBase
 
-class Geolocation(ComponentBase):
-    def __init__(self):
-        super().__init__()
+def get_geolocation():
+    key = "c43c377d6b6b4b05b1750841e52a8473"
+    response = requests.get("https://api.ipgeolocation.io/ipgeo?apiKey=" + key)
+    return response.json()
 
-geolocation = Geolocation()
+if st.button('Mi ubicación'):
+    get_geolocation()
 
-st.title("Geolocalización del Usuario en Streamlit")
 
-if st.button("Obtener Geolocalización"):
-    geolocation._update_widget_state(data={})
-    st.write("Esperando la geolocalización...")
-
-# Código JavaScript para obtener la geolocalización
-javascript_code = """
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        var locationData = { latitude, longitude };
-        // Envía los datos de latitud y longitud al servidor de Streamlit
-        Streamlit.setComponentValue(locationData);
-    });
-} else {
-    alert("Tu navegador no admite geolocalización.");
-}
-"""
-
-# Agrega el código JavaScript a la página
-st.markdown(javascript_code, unsafe_allow_html=True)
-
-# Escucha los datos de geolocalización obtenidos
-location_data = geolocation._get_value()
-if location_data:
-    latitude = location_data.get("latitude", 0)
-    longitude = location_data.get("longitude", 0)
-    st.write(f"Latitud: {latitude:.6f}")
-    st.write(f"Longitud: {longitude:.6f}")
