eb66f30136c6133d754a0d23ae5061f7800aca21
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 14:59:06 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,43 +1,7 @@
 import streamlit as st
-import json
+import streamlit_js_eval
 
-st.title("Geolocalización del Usuario en Streamlit")
-
-# Función para obtener la geolocalización del usuario en JavaScript
-javascript_code = """
-<script>
-function getUserLocation() {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-
-            // Envía los datos de latitud y longitud al servidor de Streamlit
-            var locationData = {latitude, longitude};
-            Shiny.setInputValue('locationData', locationData);
-        });
-    } else {
-        alert("Tu navegador no admite geolocalización.");
-    }
-}
-</script>
-"""
-
-# Agrega el código JavaScript a la página
-st.markdown(javascript_code, unsafe_allow_html=True)
-
-# Botón para obtener la geolocalización del usuario
-if st.button("Obtener Geolocalización"):
-    # Ejecuta la función JavaScript para obtener la geolocalización
-    st.markdown('<button onclick="getUserLocation()">Obtener ubicación</button>', unsafe_allow_html=True)
-    
-    # Escucha los datos de geolocalización obtenidos
-    location_data = st.text_area("Datos de Geolocalización", value="")
-    if location_data:
-        location_data = json.loads(location_data)
-        latitude = location_data.get("latitude", 0)
-        longitude = location_data.get("longitude", 0)
-        st.write(f"Latitud: {latitude:.6f}")
-        st.write(f"Longitud: {longitude:.6f}")
-    else:
-        st.warning("La geolocalización no está habilitada o no se pudo obtener.")
+# Returns user's location after asking for permission when the user clicks the generated link with the given text
+location = get_geolocation()
+# The URL parts of the page
+location_json = get_page_location()
