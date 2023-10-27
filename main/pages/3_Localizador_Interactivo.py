7b2382f1d80448dc282fe7b484c01b1cc6215800
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 16:18:23 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -4,7 +4,7 @@ import requests
 def get_geolocation():
     key = "c43c377d6b6b4b05b1750841e52a8473"
     response = requests.get("https://api.ipgeolocation.io/ipgeo?apiKey=" + key)
-    return response.json()
+    return st.write(response.json())
 
 if st.button('Mi ubicación'):
     get_geolocation()
