1cc80b2eb4542663e1c9ee8db4d3568e60f5db32
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 13:40:05 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-
 st.title("Interactive Map")
 
 col1, col2 = st.columns([4, 1])
@@ -9,12 +8,15 @@ options = list(leafmap.basemaps.keys())
 index = options.index("OpenTopoMap")
 
 with col2:
-
     basemap = st.selectbox("Tipo de mapa:", options, index)
 
-
 with col1:
-
     m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
     m.add_basemap(basemap)
     m.to_streamlit(height=700)
+
+    # Función para capturar el evento de clic y obtener las coordenadas
+    click_coords = m.get_click_latlon()
+    if click_coords:
+        lat, lon = click_coords
+        st.write(f"Latitud: {lat}, Longitud: {lon}")
