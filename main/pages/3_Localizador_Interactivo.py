abbf8220114630827a4e3b30543b3e5e95436ac8
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 13:50:38 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,31 +1,19 @@
 import streamlit as st
-import folium
-from streamlit_folium import folium_static
-from folium.plugins import Draw
+import leafmap.foliumap as leafmap
 
 st.title("Interactive Map")
 
 col1, col2 = st.columns([4, 1])
-
-# Opciones de mapas base
-options = ["OpenStreetMap", "Stamen Terrain", "Stamen Toner", "Stamen Watercolor", "CartoDB positron", "CartoDB dark_matter"]
-index = options.index("OpenStreetMap")
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
 
 with col2:
-    basemap = st.selectbox("Tipo de mapa:", options, index)
 
-m = folium.Map(location=[40.4168, -3.7038], zoom_start=10, tiles=basemap)
+    basemap = st.selectbox("Select a basemap:", options, index)
 
-# Añadimos la herramienta de dibujo al mapa
-draw = Draw(draw_options={'polyline': False, 'rectangle': False, 'polygon': False, 'circle': False, 'circlemarker': False})
-draw.add_to(m)
 
-# Mostrar el mapa en Streamlit
-folium_static(m)
+with col1:
 
-# Captura las coordenadas dibujadas en el mapa
-drawn_shapes = st.session_state.get('drawn_shapes', None)
-if drawn_shapes:
-    if "geometry" in drawn_shapes[-1]:
-        coords = drawn_shapes[-1]["geometry"]["coordinates"]
-        st.write(f"Latitud: {coords[1]}, Longitud: {coords[0]}")
+    m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=False, minimap_control=True)
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
