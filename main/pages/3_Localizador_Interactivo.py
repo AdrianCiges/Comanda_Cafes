a58763553c1a28026918ee02d60f933e8b07f19b
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 14:43:19 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,22 +1,22 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
-import pandas as pd
+import folium
 
 st.title("Interactive Map")
 
 col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
+options = ["OpenStreetMap", "Stamen Terrain", "Stamen Toner", "Stamen Watercolor", "CartoDB positron", "CartoDB dark_matter"]
+index = options.index("OpenStreetMap")
 
 with col2:
     basemap = st.selectbox("Select a basemap:", options, index)
 
 with col1:
-    m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
-    m.add_basemap(basemap)
+    m = folium.Map(location=[40.4168, -3.7038], zoom_start=10, tiles=basemap)
 
-# Supongamos que tienes un DataFrame llamado df con latitudes y longitudes
+# Supongamos que tienes un DataFrame llamado df con latitudes, longitudes y nombres de ubicaciones
 # Por ejemplo:
+import pandas as pd
+
 df = pd.DataFrame({
     'latitude': [40.4168, 40.4070, 40.3999],
     'longitude': [-3.7038, -3.6950, -3.6860],
@@ -25,7 +25,7 @@ df = pd.DataFrame({
 
 # Agregar marcadores para cada ubicación en el DataFrame
 for idx, row in df.iterrows():
-    leafmap.Marker(location=(row['latitude'], row['longitude']), popup=row['name']).add_to(m)
+    folium.Marker([row['latitude'], row['longitude']], popup=row['name']).add_to(m)
 
 # Mostrar el mapa en Streamlit
-m.to_streamlit(height=700)
+st.folium_static(m)
