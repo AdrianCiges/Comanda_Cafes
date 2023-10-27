11c4c00fb06207ce10273d55f46dea86ff48f185
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 16:08:24 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,19 +1,40 @@
 import streamlit as st
+from streamlit.components.v1 import ComponentBase
 
-# Título de la aplicación
+class Geolocation(ComponentBase):
+    def __init__(self):
+        super().__init__()
+
+geolocation = Geolocation()
+
 st.title("Geolocalización del Usuario en Streamlit")
 
-# Función para obtener la geolocalización del usuario
-def get_user_location():
-    if st.button("Obtener Geolocalización"):
-        user_location = st.location()
-        if user_location:
-            latitude = user_location.latitude
-            longitude = user_location.longitude
-            st.write(f"Latitud: {latitude:.6f}")
-            st.write(f"Longitud: {longitude:.6f}")
-        else:
-            st.warning("La geolocalización no está habilitada o no se pudo obtener.")
+if st.button("Obtener Geolocalización"):
+    geolocation._update_widget_state(data={})
+    st.write("Esperando la geolocalización...")
 
-# Llama a la función para obtener la geolocalización
-get_user_location()
+# Código JavaScript para obtener la geolocalización
+javascript_code = """
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var locationData = { latitude, longitude };
+        // Envía los datos de latitud y longitud al servidor de Streamlit
+        Streamlit.setComponentValue(locationData);
+    });
+} else {
+    alert("Tu navegador no admite geolocalización.");
+}
+"""
+
+# Agrega el código JavaScript a la página
+st.markdown(javascript_code, unsafe_allow_html=True)
+
+# Escucha los datos de geolocalización obtenidos
+location_data = geolocation._get_value()
+if location_data:
+    latitude = location_data.get("latitude", 0)
+    longitude = location_data.get("longitude", 0)
+    st.write(f"Latitud: {latitude:.6f}")
+    st.write(f"Longitud: {longitude:.6f}")
