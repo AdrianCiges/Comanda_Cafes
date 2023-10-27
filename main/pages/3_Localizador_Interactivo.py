cf598b474c826242da6065f4929d1f8f5f5cea8a
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 14:42:05 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pandas as pd
 
 st.title("Interactive Map")
 
@@ -8,15 +9,23 @@ options = list(leafmap.basemaps.keys())
 index = options.index("OpenTopoMap")
 
 with col2:
-
     basemap = st.selectbox("Select a basemap:", options, index)
 
-
 with col1:
-
     m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
     m.add_basemap(basemap)
-    m.to_streamlit(height=700)
 
+# Supongamos que tienes un DataFrame llamado df con latitudes y longitudes
+# Por ejemplo:
+df = pd.DataFrame({
+    'latitude': [40.4168, 40.4070, 40.3999],
+    'longitude': [-3.7038, -3.6950, -3.6860],
+    'name': ['Location 1', 'Location 2', 'Location 3']
+})
 
+# Agregar marcadores para cada ubicación en el DataFrame
+for idx, row in df.iterrows():
+    leafmap.Marker(location=(row['latitude'], row['longitude']), popup=row['name']).add_to(m)
 
+# Mostrar el mapa en Streamlit
+m.to_streamlit(height=700)
