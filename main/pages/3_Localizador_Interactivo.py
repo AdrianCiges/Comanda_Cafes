715102c4f0c0e0857b6ca952b549327a0571065f
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-27 14:07:41 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -16,4 +16,4 @@ with col1:
 
     m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
     m.add_basemap(basemap)
-    m.to_streamlit(height=700)
+    m.to_streamlit(height=900)
