9565db33ec4e886f9fd3c7a28097aa882d5333c7
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-28 21:24:18 +0200

    Update 2_Localizador.py

--- main/pages/2_Localizador.py
+++ main/pages/2_Localizador.py
@@ -20,7 +20,7 @@ from geopy.geocoders import Nominatim
 import urllib.parse
 import geocoder
 import os
-
+import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide", page_title="Ruta del Café", page_icon="./img/cafe5.png")
 
@@ -232,6 +232,28 @@ def get_data():
 
 # ---------------------------------------------------------------------------------FUNCIONES⬆️-------------------------------------
 # -------------------------------------------------------------------------------UBI A MANO ⬇️-------------------------------------
+
+with st.expander('Encontrar mi ubicación', expanded=False):   
+        
+    # col1, col2 = st.columns([4, 1])
+    # options = list(leafmap.basemaps.keys())
+    # index = options.index("OpenTopoMap")
+    
+    # with col2:
+    
+    #     basemap = st.selectbox("Select a basemap:", options, index)
+    
+    
+    # with col1:
+    
+    m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
+
+
+# -------------------------------------------------------------------------------UBI A MANO ⬆️-------------------------------------
+# ---------------------------------------------------------------------------------MAPEANDO ⬇️-------------------------------------
+
 
 layout = st.sidebar.columns([1, 1])
 
