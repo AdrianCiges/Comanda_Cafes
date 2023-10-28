5a01cfea1a5caa15f8f698cc234e967bf848d76f
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-28 20:40:22 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import folium
+from streamlit_folium import folium_static
 
 st.title("Interactive Map")
 
@@ -7,20 +8,31 @@ st.title("Interactive Map")
 coordinates = []
 
 col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
+options = ["OpenStreetMap", "OpenTopoMap", "Stamen Terrain"]
 index = options.index("OpenTopoMap")
 
 with col2:
-    basemap = st.selectbox("Select a basemap:", options, index)
+    basemap_choice = st.selectbox("Select a basemap:", options, index)
 
+# Creamos un mapa usando folium
+m = folium.Map(location=[20, 0], zoom_start=2)
+
+# Añadimos el basemap elegido
+if basemap_choice == "OpenTopoMap":
+    folium.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', name="OpenTopoMap", attr="OpenTopoMap").add_to(m)
+elif basemap_choice == "Stamen Terrain":
+    folium.TileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', name="Stamen Terrain", attr="Stamen").add_to(m)
+
+# Función para capturar las coordenadas en un popup al hacer clic
+def add_marker(map, location):
+    coordinates.append(location)
+    folium.Marker(location, popup=f"Coordinates: {location[0]}, {location[1]}").add_to(map)
+
+m.add_child(folium.ClickForMarker(popup=add_marker))
+
+# Mostramos el mapa en streamlit
 with col1:
-    m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
-    m.add_basemap(basemap)
-    m.to_streamlit(height=700)
+    folium_static(m)
 
-    # Captura el evento 'click' y almacena las coordenadas
-    def on_map_click(e):
-        coordinates.append((e.latlng.lat, e.latlng.lng))
-        st.write(f"Coordinates: {e.latlng.lat}, {e.latlng.lng}")
-
-    m.on('click', on_map_click)
+for coord in coordinates:
+    st.write(f"Coordinates: {coord[0]}, {coord[1]}")
