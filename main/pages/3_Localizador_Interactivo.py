aec57c39b846bce5ee9d15f9c6a25104262ef378
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-28 21:00:51 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -7,17 +7,25 @@ def get_ip():
     return response["ip"]
 
 
-def get_location():
-    ip_address = get_ip()
-    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-    location_data = {
-        "ip": ip_address,
-        "city": response.get("city"),
-        "region": response.get("region"),
-        "country": response.get("country_name")
-    }
-    return location_data
+# def get_location():
+#     ip_address = get_ip()
+#     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+#     location_data = {
+#         "ip": ip_address,
+#         "city": response.get("city"),
+#         "region": response.get("region"),
+#         "country": response.get("country_name")
+#     }
+#     return location_data
 
 
 if st.button('Mi ubicación'):
-    st.write(get_location())
+    # st.write(get_location())
+
+    ip_address = get_ip()
+    request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+    response = requests.get(request_url)
+    result = response.content.decode()
+    result = result.split("(")[1].strip(")")
+    result  = json.loads(result)
+    st.write(result)
