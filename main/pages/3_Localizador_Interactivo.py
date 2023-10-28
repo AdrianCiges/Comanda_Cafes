1ada685736cc4eab36ba2ced72c8a6f242a2cf5c
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-28 20:48:07 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,38 +1,22 @@
 import streamlit as st
-import folium
-from streamlit_folium import folium_static
+import leafmap.foliumap as leafmap
 
-st.title("Interactive Map")
+def get_user_location():
+    m = leafmap.Map()
+    
+    # Añadimos la función de geolocalización
+    locate_control = leafmap.LocateControl(auto_start=True, fly_to=True)
+    m.add_control(locate_control)
 
-# Lista para almacenar las coordenadas capturadas
-coordinates = []
+    # Aquí puedes añadir código adicional para mostrar el mapa en Streamlit si lo deseas
+    # Por ejemplo: m.to_streamlit()
 
-col1, col2 = st.columns([4, 1])
-options = ["OpenStreetMap", "OpenTopoMap", "Stamen Terrain"]
-index = options.index("OpenTopoMap")
+    # Devuelve las coordenadas del usuario
+    return locate_control.last_location
 
-with col2:
-    basemap_choice = st.selectbox("Select a basemap:", options, index)
-
-# Creamos un mapa usando folium
-m = folium.Map(location=[20, 0], zoom_start=2)
-
-# Añadimos el basemap elegido
-if basemap_choice == "OpenTopoMap":
-    folium.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', name="OpenTopoMap", attr="OpenTopoMap").add_to(m)
-elif basemap_choice == "Stamen Terrain":
-    folium.TileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', name="Stamen Terrain", attr="Stamen").add_to(m)
-
-# Función para capturar las coordenadas en un popup al hacer clic
-def add_marker(map, location):
-    coordinates.append(location)
-    folium.Marker(location, popup=f"Coordinates: {location[0]}, {location[1]}").add_to(map)
-
-m.add_child(folium.ClickForMarker(popup=add_marker))
-
-# Mostramos el mapa en streamlit
-with col1:
-    folium_static(m)
-
-for coord in coordinates:
-    st.write(f"Coordinates: {coord[0]}, {coord[1]}")
+if st.button("Get my location"):
+    location = get_user_location()
+    if location:
+        st.write(f"Your coordinates are: {location}")
+    else:
+        st.write("Unable to get your location.")
