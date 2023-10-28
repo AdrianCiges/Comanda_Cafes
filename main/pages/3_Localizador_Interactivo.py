d6bdfd34395f752af56d32fdc6fcc6e594a63c48
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-28 20:56:38 +0200

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,18 +1,21 @@
-import streamlit as st
 import requests
 
-def get_location_from_ip():
-    try:
-        response = requests.get("https://ipinfo.io/json?token=83c178d0c646a4")
-        data = response.json()
-        location = data['loc'].split(',')
-        return float(location[0]), float(location[1])
-    except:
-        return None, None
 
-if st.button("Get my location"):
-    lat, lon = get_location_from_ip()
-    if lat and lon:
-        st.write(f"Your approximate coordinates are: Latitude: {lat}, Longitude: {lon}")
-    else:
-        st.write("Unable to get your location.")
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return location_data
+
+
+print(get_location())
