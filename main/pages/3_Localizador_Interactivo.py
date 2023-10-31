4946d3ec2ccc8b3bc8f242dcfbffd46862cddfca
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-10-31 16:45:11 +0100

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,35 +1,4 @@
 import streamlit as st
-import requests
-import json
-from Flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def get_ip():
-    user_ip = request.remote_addr
-    return f"Your IP address is: {user_ip}"
-
-
-# def get_location():
-#     ip_address = get_ip()
-#     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-#     location_data = {
-#         "ip": ip_address,
-#         "city": response.get("city"),
-#         "region": response.get("region"),
-#         "country": response.get("country_name")
-#     }
-#     return location_data
-
-
-if st.button('Mi ubicación'):
-    # st.write(get_location())
-
-    ip_address = get_ip()
-    request_url = 'https://geolocation-db.com/jsonp/' + ip_address
-    response = requests.get(request_url)
-    result = response.content.decode()
-    result = result.split("(")[1].strip(")")
-    result  = json.loads(result)
-    st.write(result)
+import geocoder
+g = geocoder.ip('me')
+st.write(g.latlng)
