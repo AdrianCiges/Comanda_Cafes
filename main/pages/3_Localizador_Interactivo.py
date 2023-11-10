1d539a360e2f44958e4142bc53dde914490526ae
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-11-10 15:55:38 +0100

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -2,8 +2,7 @@ import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 import json
 
-def get_user_loc():
-    loc = get_geolocation()
-    return [loc['coords']['latitude'], loc['coords']['longitude']]
+loc = get_geolocation()
+st.write(f'{loc})
+st.write(f'{loc['coords']['latitude']}, {loc['coords']['longitude']}]
 
-get_user_loc()
