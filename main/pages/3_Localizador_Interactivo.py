bd51054d0a4b95fc5b20180f3d676247775e3500
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-11-10 14:21:29 +0100

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,4 +1,26 @@
-import streamlit as st
 import geocoder
-g = geocoder.ip('me')
-st.write(g.latlng)
+import folium
+
+def obtener_ubicacion_actual():
+    # Obtener la ubicación basada en la dirección IP
+    g = geocoder.ip('me')
+    return g.latlng
+
+def mostrar_mapa(ubicacion):
+    # Crear un mapa centrado en la ubicación actual
+    mapa = folium.Map(location=ubicacion, zoom_start=15)
+
+    # Agregar un marcador para la ubicación actual
+    folium.Marker(ubicacion, popup='Ubicación Actual').add_to(mapa)
+
+    # Mostrar el mapa
+    display(mapa)
+
+# Obtener la ubicación actual
+ubicacion_actual = obtener_ubicacion_actual()
+
+# Mostrar el mapa con la ubicación actual
+if ubicacion_actual:
+    mostrar_mapa(ubicacion_actual)
+else:
+    print("No se pudo obtener la ubicación actual.")
