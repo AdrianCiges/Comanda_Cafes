d06e1e2703acbaaaf8c0025ad4af8064d0155496
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-11-10 15:23:56 +0100

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -6,14 +6,11 @@ from streamlit_js_eval import streamlit_js_eval
 width = streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')
 
 
-
 def disp_width():
     to_display = f"Screen width is _{width}_"
     
     print(to_display)
 
 
-
-
-
 st.button("Display Screen Width", on_click=disp_width)
+disp_width()
