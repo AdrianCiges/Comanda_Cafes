6df075598abf6b065e79aa21a68fbe9fbc8bce22
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-11-10 15:27:41 +0100

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -1,16 +1,13 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-from streamlit_js_eval import streamlit_js_eval
+def map_widget():
+    st.title("Seleccione su Ubicación en el Mapa")
+    components.iframe("https://www.openstreetmap.org", width=700, height=450)
+    lat = st.number_input("Latitud", format="%f")
+    lon = st.number_input("Longitud", format="%f")
+    if st.button("Confirmar Ubicación"):
+        st.write("Latitud:", lat, "Longitud:", lon)
 
-
-width = streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')
-
-
-def disp_width():
-    to_display = f"Screen width is _{width}_"
-    
-    print(to_display)
-
-
-st.button("Display Screen Width", on_click=disp_width)
-disp_width()
+if __name__ == "__main__":
+    map_widget()
