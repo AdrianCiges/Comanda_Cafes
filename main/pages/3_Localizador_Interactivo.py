f1d2513fb55ee5af54682c7005b071f9e15e350c
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-11-10 16:17:28 +0100

    Update 3_Localizador_Interactivo.py

--- main/pages/3_Localizador_Interactivo.py
+++ main/pages/3_Localizador_Interactivo.py
@@ -2,9 +2,19 @@ import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 import json
 
+# loc = get_geolocation()
+# # st.write(f'{loc}')
+# latitud = list(loc['coords']['latitude'])[0]
+# longitud = list(loc['coords']['longitude'])[0]
+# st.write(f'{latitud}, {longitud}')
+
 loc = get_geolocation()
-# st.write(f'{loc}')
-latitud = list(loc['coords']['latitude'])[0]
-longitud = list(loc['coords']['longitude'])[0]
-st.write(f'{latitud}, {longitud}')
+
+if loc is not None:
+    latitud = list(loc['coords']['latitude'])[0]
+    longitud = list(loc['coords']['longitude'])[0]
+    st.write(f'{latitud}, {longitud}')
+else:
+    st.write("No se pudo obtener la ubicación.")
+
 
