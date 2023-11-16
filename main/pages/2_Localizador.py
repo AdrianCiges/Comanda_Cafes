094cb330c86aa0ebec9c6c89fd18845574959b38
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-11-16 19:19:54 +0100

    Update 2_Localizador.py

--- main/pages/2_Localizador.py
+++ main/pages/2_Localizador.py
@@ -179,9 +179,9 @@ num_cafeterias = st.sidebar.number_input("Nº de cafeterías", value=10, min_val
 from_pc = st.sidebar.checkbox('Vista para ordenador')
 
 if from_pc:
-    size_map = 1000
-else:
     size_map = 380
+else:
+    size_map = 1100
 
 
 if num_cafeterias != 1:
