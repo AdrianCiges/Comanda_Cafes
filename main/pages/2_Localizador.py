e3fc4e6fb1be2e73a8a42f34429cc676afd9724a
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2024-02-21 18:05:20 +0100

    Update 2_Localizador.py

--- main/pages/2_Localizador.py
+++ main/pages/2_Localizador.py
@@ -146,11 +146,6 @@ def haversine_distance(lat1, lon1, lat2, lon2):
 
 @st.cache_data
 def get_data():
-    my_bar = st.progress(0)
-    for percent_complete in range(100):
-        timee.sleep(0.1)
-        my_bar.progress(percent_complete +1)
-    
     data_url = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'conjunto_cafes_nodupl.xlsx')
     df = pd.read_excel(data_url)
     df = df.drop('Columna1', axis=1)
