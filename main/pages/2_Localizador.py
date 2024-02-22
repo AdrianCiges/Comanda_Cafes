7d6e1a242aecd1f66b3bc175250cc6faafc8a154
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2024-02-22 14:32:31 +0100

    Update 2_Localizador.py

--- main/pages/2_Localizador.py
+++ main/pages/2_Localizador.py
@@ -146,7 +146,7 @@ def haversine_distance(lat1, lon1, lat2, lon2):
 
 @st.cache_data
 def get_data():
-    data_url = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'cafeterias_horarios_ocupacionxlsx')
+    data_url = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'cafeterias_horarios_ocupacion.xlsx')
     df = pd.read_excel(data_url)
     df = df.drop('Columna1', axis=1)
     return df
