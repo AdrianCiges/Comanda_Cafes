ba10fa8c5d6540017590b7bb2b674438ba5e4a0b
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2024-02-26 17:40:49 +0100

    Update 2_Localizador.py

--- main/pages/2_Localizador.py
+++ main/pages/2_Localizador.py
@@ -613,7 +613,7 @@ if st.checkbox('📍 Usar mi ubicación'):
         st.write(10/0) # Provocamos el error
     except:
         st.error('No hemos podido acceder a tu ubicación. Selecciona tu municipio en el siguiente desplegable para buscar tu cafetería ideal:', icon="⚠️")
-        ciudad_seleccionada = st.selectbox('Selecciona una ciudad', options=list(dictio_coords_saviour.keys()),placeholder="Busca tu ubicación más cercana para un relaxing cup of café con leche", index=25)
+        ciudad_seleccionada = st.selectbox('Selecciona una ciudad', options=list(dictio_coords_saviour.keys()),placeholder="Busca tu ubicación más cercana para un relaxing cup of café con leche", index=50)
         if ciudad_seleccionada:
             latitud = round(float(dictio_coords_saviour[ciudad_seleccionada].split(', ')[0]), 4)
             longitud = round(float(dictio_coords_saviour[ciudad_seleccionada].split(', ')[1]), 4)
