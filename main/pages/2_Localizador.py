0a8f091ccf7aac95adf1e1bb3cfe4f57cf8bc0b8
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2024-02-26 18:30:58 +0100

    Update 2_Localizador.py

--- main/pages/2_Localizador.py
+++ main/pages/2_Localizador.py
@@ -100,34 +100,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown("""
-                <html>
-                    <head>
-                    <style>
-                        ::-webkit-scrollbar {
-                            width: 10px;
-                            }
-
-                            /* Track */
-                            ::-webkit-scrollbar-track {
-                            background: #f1f1f1;
-                            }
-
-                            /* Handle */
-                            ::-webkit-scrollbar-thumb {
-                            background: #888;
-                            }
-
-                            /* Handle on hover */
-                            ::-webkit-scrollbar-thumb:hover {
-                            background: #555;
-                            }
-                    </style>
-                    </head>
-                    <body>
-                    </body>
-                </html>
-            """, unsafe_allow_html=True)
 # ------------------------------------------------------------------------------------CONFIG⬆️-------------------------------------
 # ---------------------------------------------------------------------------------FUNCIONES⬇️-------------------------------------
 
